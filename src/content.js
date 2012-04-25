b75e4518b4dafbf665baa1c5f2c8edcc5b58e2c3
Paul Sweeney <psweeney@beachmint.com>
2012-04-25 10:49:09 -0700

    Adding less support

--- src/content.js
+++ src/content.js
@@ -516,6 +516,7 @@ LivereloadContent.prototype['.js'] = LivereloadContent.prototype.handleJS;
 LivereloadContent.prototype['.css'] = LivereloadContent.prototype.handleCSS;
 LivereloadContent.prototype['.sass'] = LivereloadContent.prototype.handleCSS;
 LivereloadContent.prototype['.scss'] = LivereloadContent.prototype.handleCSS;
+LivereloadContent.prototype['.less'] = LivereloadContent.prototype.handleCSS;
 LivereloadContent.prototype['.jpg'] =
 LivereloadContent.prototype['.jpeg'] =
 LivereloadContent.prototype['.png'] =
